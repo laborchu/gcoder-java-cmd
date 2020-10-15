b5b961a42424fa27e07f0635250ed4eb060f9af2
yangtaisu <yangcjcjlu@126.com>
2020-10-15 15:20:30 +0800

    fix/根据现有项目结构重新修改gcoder

--- template/cmd/${upperCamelName}DelCmd.java
+++ template/cmd/${upperCamelName}DelCmd.java
@@ -2,8 +2,8 @@ package ${config.java.package.cmd}.${table.prefix};
 
 import ${config.java.package.cmd}.CmdSign;
 import ${config.java.package.code}.CmdCode;
-import ${config.java.package.vo}.${table.prefix}.${table.upperCamelName}VO;
-import ${config.java.package.service}.${table.prefix}.I${table.upperCamelName}Service;
+import ${config.java.package.bo}.${table.prefix}.${table.upperCamelName}BO;
+import ${config.java.package.service}.${table.prefix}.service.I${table.upperCamelName}Service;
 import ${config.java.package.cmd}.ACmd;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
---
+++ template/vo/${upperCamelName}BO.java
@@ -0,0 +1,10 @@
+package ${config.java.package.vo}.${table.prefix};
+
+import ${config.java.package.vo}.${table.prefix}.${table.upperCamelName}VO;
+
+import lombok.Data;
+
+@Data
+public class ${table.upperCamelName}BO extends ${table.upperCamelName}VO{
+
+}
